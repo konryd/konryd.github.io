b61a4fa3b9c5e1633fac384e7e5edfa7121bde6c
Konrad Delong <konryd@google.com>
2024-10-26 19:56:21 +0200

    added servieworker

---
+++ feedback/sw.js
@@ -0,0 +1,10 @@
+var GHPATH = '/feedback';
+var APP_PREFIX = 'konryd_feedback_';
+var VERSION = 'version_00';
+ 
+// The files to make available for offline use. make sure to add 
+// others to this list
+var URLS = [    
+  `${GHPATH}/`,
+  `${GHPATH}/vote.html`,
+]
