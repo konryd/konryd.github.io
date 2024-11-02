02df58c4ea8c04ce44e472f9fde400c2ae16400a
Konrad Delong <konryd@google.com>
2024-11-02 10:19:55 +0100

    added async cache refresh even for cache hit

--- feedback/sw.js
+++ feedback/sw.js
@@ -11,19 +11,22 @@ self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request)
       .then(function(response) {
-        if (response) {
-          console.log('Serving from cache:', event.request.url);
-          return response; 
-        } else {
-          console.log('Fetching from network:', event.request.url);
-          return fetch(event.request)
+        var fetchPromise = fetch(event.request, {cache: "reload"})
             .then(function(response) {
+              console.log('Fetched from network:', event.request.url);
               return caches.open('konryd_feedback_cache')
                 .then(function(cache) {
                   cache.put(event.request, response.clone()); 
                   return response;
                 });
             });
+
+        if (response) {
+          console.log('Serving from cache:', event.request.url);
+          event.waitUntil(fetchPromise);
+          return response; 
+        } else {
+          return fetchPromise;
         }
       })
   );
